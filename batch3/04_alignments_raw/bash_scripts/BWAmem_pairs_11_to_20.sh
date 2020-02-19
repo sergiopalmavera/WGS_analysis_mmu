88698c296668877d0060063db21a887ea33f3dbd
sergiopalmavera <palma-vera@linu4.fbn-dummerstorf.de>
2020-02-19 11:41:29 +0100

    alignment scripts pairs 11 to 50

---
+++ batch3/04_alignments_raw/bash_scripts/BWAmem_pairs_11_to_20.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Set number of pairs 
+FROM=11
+TO=20
+
+# Print version of programs
+echo "# bwa version" # for bwa there is apparently no argument to query the version
+bwa &> fl.tmp
+grep Version fl.tmp
+rm fl.tmp
+
+printf "\n"
+
+echo "# Samtools version"
+samtools --version
+
+printf "\n"
+
+# Set number of threads
+THR=20
+
+# Set path to reference genome (relative path)
+REF=../../../reference_genome_ensembl
+
+# Set path to fastq "fastp-corrected" files
+FASTQ=../../02_quality_trimming_adapter_removal/output
+
+# Define name of R1 files
+R1s=$(ls -1 $FASTQ | grep "fastq.gz" | grep "R1" | sed -n $FROM,${TO}p )
+
+# Print a message with the list of pairs being processed
+for R1 in $R1s; do echo "# Processing pairs ${R1/R1/[R1,R2]}"; done
+printf "\n"
+
+# Loop over each R1 file
+for R1 in $R1s
+do
+	# Print a message about the read pair being processed
+	echo "## Processing pair ${R1/R1/[R1,R2]}"
+
+	# Define R2 fastq file
+	R2=$(echo ${R1/R1/R2})
+
+	# Print a message with both fastq files in pair
+	echo "### R1: $R1"
+	echo "### R2: $R2"
+	
+	# Define name of BAM file	
+	FLNM=$(echo $R1 | cut -d'_' -f1,2,3 | cut -d'.' -f1)
+	
+	# Run bwa-mem on fastq pair and pipe output to samtools to convert sam to bam
+	echo "### Starting bwa mem"
+	time bwa mem -t $THR -M $REF/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz $FASTQ/$R1 $FASTQ/$R2 | samtools view -@ $THR -bS - > ../output/$FLNM.bam 
+	printf "\n"
+
+	echo "### Process completed, this is no confirmation that all went well, you need to inspect the BAM files!"
+done
