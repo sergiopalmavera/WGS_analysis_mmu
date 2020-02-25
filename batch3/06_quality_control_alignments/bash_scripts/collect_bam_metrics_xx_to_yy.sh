ce8507db03a4e867fe3c6872f4ee6d383f9b8374
sergiopalmavera <palma-vera@linu3.fbn-dummerstorf.de>
2020-02-25 12:24:18 +0100

    bash scripts collect bam metrics

---
+++ batch3/06_quality_control_alignments/bash_scripts/collect_bam_metrics_xx_to_yy.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+FROM
+TO
+
+echo "# Working on:"
+hostname
+printf "\n"
+
+# Retrieving metrics from bams sorted-AddRG-dedup-bqsr
+
+# Absoulute path (modify accordingly)
+picard=~/FBN_HOME/Tools/picard_2.18.11
+
+bams=$(ls -1 ../../05_alignments_addRG_dedup_bqsr/output/*.bam | sed -n $FROM,${TO}p)
+echo "# Processing samples:"
+for b in $bams; do echo $b; done
+printf "\n"
+
+for b in $bams
+do
+	bam=$(basename $b)
+	echo "## Processing $bam"
+	printf "\n"
+
+	echo "### samtools: flagstat ..."
+	time samtools flagstat $b > ../output/${bam/.bam/.flagstat.tab}
+	printf "\n"
+
+	echo "### picard: BamIndexStats ..."
+	time java -jar $picard/picard.jar BamIndexStats I=$b > ../output/${bam/.bam/.BAMIndexStats.tab}
+	printf "\n"
+
+	echo "### CollectWgsMetrics default ..."
+	java -jar $picard/picard.jar CollectWgsMetrics \
+		I=$b \
+		O=../output/${bam/.bam/.CollectWgsMetrics.default.txt} \
+		R=../../../reference_genome_ensembl/Mus_musculus.GRCm38.dna.primary_assembly.fa
+	printf "\n"
+
+	echo "### CollectWgsMetrics no filters..."
+	java -jar $picard/picard.jar CollectWgsMetrics \
+		I=$b \
+		O=../output/${bam/.bam/.CollectWgsMetrics.Q0.M0.txt} \
+		R=../../../reference_genome_ensembl/Mus_musculus.GRCm38.dna.primary_assembly.fa \
+		Q=0 \
+		MQ=0
+	printf "\n"
+	
+	echo "### picard: CollectInsertSizeMetrics ..."
+	java -jar $picard/picard.jar CollectInsertSizeMetrics \
+		I=$b \
+		O=../output/${bam/.bam/.CollectInsertSizeMetrics.txt} \
+		H=../output/${bam/.bam/.CollectInsertSizeMetrics.pdf}
+	printf "\n"
+
+done
---
+++ batch3/06_quality_control_alignments/bash_scripts/make_script.sh
@@ -0,0 +1,18 @@
+# Take the example script and change it according to the desired batch of samples
+
+
+FROM=$1
+TO=$2
+
+script_nm=collect_bam_metrics_xx_to_yy.sh
+
+fl_nm=$(echo $script_nm | sed "s/xx/$FROM/;s/yy/$TO/")
+
+sed -z "s/FROM/FROM=$FROM/ ; s/TO/TO=$TO/" $script_nm > $fl_nm # -z to match only first instance
+
+chmod +x ./$fl_nm
+
+nohup ./$fl_nm &> ./${fl_nm/.sh/.out} &
+
+
+
