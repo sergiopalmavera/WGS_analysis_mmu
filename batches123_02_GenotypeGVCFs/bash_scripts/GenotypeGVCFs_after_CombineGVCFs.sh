75e20aa91c857367f4711b73f1f0c008f0ba0dd8
sergiopalmavera <palma-vera@linu2.fbn-dummerstorf.de>
2020-03-30 09:21:57 +0200

    GenotypeGVCFs_after_CombineGVCFs and LogDoc updated

--- batches123_02_GenotypeGVCFs/bash_scripts/GenotypeGVCFs_after_CombineGVCFs.sh
+++ batches123_02_GenotypeGVCFs/bash_scripts/GenotypeGVCFs_after_CombineGVCFs.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-chr=13
+echo "# Printing host and date..."
+hostname
+date
+printf "\n"
 
 # Absoulte paths (modify accordingly)
 #GATK=/home/fb4/palma-vera/FBN_HOME/Tools/gatk-4.0.6.0 # version used until HaplotypeCaller
 GATK=/home/fb4/palma-vera/FBN_HOME/Tools/gatk-4.1.5.0 # keep on using same version as for ConsolidateGVCFs (less bugs)
 
 # Relative paths
-DB=../../batches123_01_ConsolidateGVFs/chr$chr
 REF=../../reference_genome_ensembl
 OUT=../output
-TMP_DIR=./TMP/chr$13
-
-mkdir $TMP_DIR
+GVCF=/projekte/I2-SOS-FERT/GitHub/WGS_analysis_mmu/batches123_01_CombineGVFs/output
 
 $GATK/gatk GenotypeGVCFs \
 	-R $REF/Mus_musculus.GRCm38.dna.primary_assembly.fa \
-	-V gendb://$DB \
-	-O $OUT/chr${chr}.vcf.gz \
-	--tmp-dir=$TMP_DIR \
+	-V $GVCF/cohort.g.vcf \
+	-O $OUT/cohort.vcf \
 	--allow-old-rms-mapping-quality-annotation-data
-
-rm -r $TMP_DIR 
