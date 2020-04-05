e0ac8a35317ea7abcbae30f38f498294ac82c8b4
sergiopalmavera <palma-vera@linu6.fbn-dummerstorf.de>
2020-04-05 16:57:27 +0200

    concatenate chr1, concatenate chr2, consolidate chrs3-5

--- batches123_01_ConsolidateGVFs/bash_scripts/make_script_chr4_to_chr19.sh
+++
@@ -1,7 +0,0 @@
-for i in $(seq 4 1 19)
-do
-	cp ConsolidateGVCFs_chr1.sh ./tmp/tmp${i} 
-	sed "s/chr=1/chr=$i/" ./tmp/tmp${i} > ./ConsolidateGVCFs_chr${i}.sh
-	nohup ./ConsolidateGVCFs_chr${i}.sh &> ./ConsolidateGVCFs_chr${i}.out &
-	rm tmp/*
-done
