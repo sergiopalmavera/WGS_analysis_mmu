f4200d95729e6174696ca338554e9015e7a4b292
sergiopalmavera <palma-vera@fbn-dummerstorf.de>
2020-06-17 17:50:51 +0200

    dashboard updated - vcf metrics and annotations

--- batches123_04_FinalVCF/scripts/01_get_mean_FORMAT_DP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pct=99.9
-
-bcftools=~/FBN_HOME/Tools/samtools_1.9/bcftools-1.9_installed/bin
-
-VAR_DIR=../../batches123_03_VariantQualityScoreRecalibration/output/
-
-$bcftools/bcftools query -f '[%DP ]\n' $VAR_DIR/cohort_biallelicSNPs_VQSR${pct}_PASS.vcf -o ../output/DP.tmp
