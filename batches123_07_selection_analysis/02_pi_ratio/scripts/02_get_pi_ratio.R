f3ce754d724b48d37059e31533055e9f3f0b2ca3
sergiopalmavera <palma-vera@linu3.fbn-dummerstorf.de>
2020-06-19 15:41:46 +0200

    pi ratio r script updated

--- batches123_07_selection_analysis/02_pi_ratio/scripts/02_get_pi_ratio.R
+++ batches123_07_selection_analysis/02_pi_ratio/scripts/02_get_pi_ratio.R
@@ -55,14 +55,76 @@ pi_ratio_du6_fztdu <- prepare_contrasts("DU6","FZTDU")
 pi_ratio_du6p_fztdu <- prepare_contrasts("DU6P","FZTDU")
 pi_ratio_duhlb_fztdu <- prepare_contrasts("DUhLB","FZTDU")
 
-# export tables
+# export tables for dashboard
 write.csv(pi_ratio_duk_fztdu, "../output/pi_ratio_duk_fztdu.csv")
 write.csv(pi_ratio_duc_fztdu, "../output/pi_ratio_duc_fztdu.csv")
 write.csv(pi_ratio_du6_fztdu, "../output/pi_ratio_du6_fztdu.csv")
 write.csv(pi_ratio_du6p_fztdu, "../output/pi_ratio_du6p_fztdu.csv")
 write.csv(pi_ratio_duhlb_fztdu, "../output/pi_ratio_duhlb_fztdu.csv")
 
+#------------------------------------------------------------
+# same as in dasboard, but discarded (mostly zeroes!!!)
+# i should've expected it, since most sites in selected lines
+# are homozygous, while most sites in fztdu are polymorphic
+#------------------------------------------------------------
+
+# load data
+fls <- list.files("../output", pattern = ".csv$", full.names = T)
+
+pi_ratio_dat <- lapply(fls, function(fl){
+  read.csv(fl) %>% dplyr::select(CHROM, BIN_START, BIN_END, pi_ratio)
+})
+
+names(pi_ratio_dat) <- basename(fls) %>% str_remove("pi_ratio_") %>% str_remove(".csv")
+
+pi_ratio_dat <- pi_ratio_dat %>% 
+  bind_rows(.id = "contrast") %>% 
+  mutate(pi_ratio_log2 = log2(pi_ratio))
+
+# summary
+summary_win_pi_ratio <- pi_ratio_dat %>% 
+  mutate(is_x = (CHROM == "X")) %>% 
+  group_by(contrast, is_x) %>% 
+  summarise(
+    n_win = n(),
+    min = min(pi_ratio),
+    q1 = quantile(pi_ratio, 0.01),
+    q5 = quantile(pi_ratio, 0.05),
+    median = median(pi_ratio),
+    mean = mean(pi_ratio),
+    max = max(pi_ratio)
+  ) %>% 
+  arrange(is_x)
+
+summary_win_pi_ratio %>%
+  mutate(is_x = ifelse(is_x, "X", "Autosome")) %>% 
+  dplyr::rename(chr_type = is_x) %>% 
+  kable(digits = 2, caption = "Mean Window pi_ratio")
 
 
+# distribution
+p1 <- pi_ratio_dat %>% 
+  ggplot(aes(x = pi_ratio)) +
+  geom_histogram(bins = 50) +
+  facet_wrap(~contrast, ncol = 1, strip.position = "right") +
+  theme_bw() +
+  xlab("pi-ratio")
+
+p2 <- pi_ratio_dat %>% 
+  ggplot(aes(x = NA, y = pi_ratio)) +
+  geom_boxplot(width = 0.2) +
+  coord_flip() +
+  facet_wrap(~contrast, ncol = 1, strip.position = "right") +
+  theme_bw() +
+  theme(
+    axis.text.y = element_blank(),
+    axis.title.y = element_blank(),
+    axis.ticks.y = element_blank()
+  ) +
+  ylab("pi-ratio")
+
+gridExtra::grid.arrange(p1,p2, nrow = 1)
+
+rm(p1,p2)
 
 
