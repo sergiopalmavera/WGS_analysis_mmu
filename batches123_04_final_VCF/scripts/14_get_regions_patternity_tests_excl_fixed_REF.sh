a774df2b3d79486b9e1a0e89c62ca145682585b7
sergiopalmavera <palma-vera@linu2.fbn-dummerstorf.de>
2020-08-05 16:05:01 +0200

    patternity test regions of interest extracted

---
+++ batches123_04_final_VCF/scripts/14_get_regions_patternity_tests_excl_fixed_REF.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Extract regions of interest for patternity test
+
+GATK=~/FBN_HOME/Tools/gatk-4.0.6.0
+vcftools=/home/fb4/palma-vera/FBN_HOME/Tools/vcftools_0.1.13/cpp
+REF=../../reference_genome_ensembl
+
+for vcf in $(ls ../output/*.vcf | grep -Ev "allrecords|filtered.vcf|withmissingness.vcf")
+do
+	echo "# $vcf"
+
+	echo "## Indexing"
+	$GATK/gatk IndexFeatureFile -F $vcf
+	
+	echo "## Creating vcf subsets"
+	$GATK/gatk SelectVariants \
+		-R $REF/Mus_musculus.GRCm38.dna.primary_assembly.fa \
+		-V $vcf \
+		-L 9:86562500-86565000 \
+		-L 17:40906500-40908000 \
+		-L 17:41101000-41103500 \
+		-O ${vcf/.vcf/_paternity.vcf} 
+	printf "\n"
+
+	echo "## Get alles in regions of interest"
+	$vcftools/vcftools --freq --vcf  ${vcf/.vcf/_paternity.vcf} --out ${vcf/.vcf/_paternity}
+	printf "\n"
+done
+
