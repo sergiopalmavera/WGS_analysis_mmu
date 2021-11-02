c036a70b905a8bf89861ddf70ebf112e220d272d
Palma Vera <palma-vera@izw-berlin.local>
2021-11-02 11:02:18 +0100

    .gitignore updated

--- resource_mgp_sanger_REL-1505-SNPs_Indels/get_mgp_v5_merged_indels.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget "ftp://ftp-mouse.sanger.ac.uk/current_snps/mgp.v5.merged.indels.dbSNP142.normed.vcf.gz"
-
-wget "ftp://ftp-mouse.sanger.ac.uk/current_snps/mgp.v5.merged.indels.dbSNP142.normed.vcf.gz.md5"
